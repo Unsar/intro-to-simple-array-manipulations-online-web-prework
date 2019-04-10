32257d76497233852cd730193530fca7e85ebab3
Unsar Malik <unsar.malik24@gmail.com>
2019-04-10 02:34:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,10 +13,6 @@ def using_pop(great_hits_of_the_nineties)
    great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
    great_hits_of_the_nineties.pop
  end
- 
- def pop(great_hits_of_the_nineties)
-   great_hits_of_the_nineties.size
- end
 
 def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
