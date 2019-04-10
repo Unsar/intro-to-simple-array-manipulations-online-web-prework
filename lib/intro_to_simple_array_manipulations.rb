c562168a7f82e0a5bc8423982c29424592e5ebcd
Unsar Malik <unsar.malik24@gmail.com>
2019-04-10 02:42:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,6 +14,8 @@ def using_pop(great_hits_of_the_nineties)
    great_hits_of_the_nineties.pop
  end
  
+puts great_hits_of_the_nineties.size
+end
 
 def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
