79625f733ae806fa060962de9c453b3ac70aea3b
Unsar Malik <unsar.malik24@gmail.com>
2019-04-10 02:43:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,7 +13,8 @@ def using_pop(great_hits_of_the_nineties)
    great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
    great_hits_of_the_nineties.pop
  end
- 
+
+def pop1(great_hits_of_the_nineties)
 puts great_hits_of_the_nineties.size
 end
 
