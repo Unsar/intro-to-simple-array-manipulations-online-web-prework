f1e5ea1c1bf164e654ecb1870f0ea85c61f90caa
Unsar Malik <unsar.malik24@gmail.com>
2019-04-10 02:28:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,7 +29,7 @@ def using_shift(my_favorite_cities)
     ice_cream_brands.shift(2)
   end
     
-  def using_concat(my_favorite_things)
+  def using_concat(my_favorite_things, more_favs)
     my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["mario kart", "flatiron school"]
     my_favorite_things.concat(more_favs)
