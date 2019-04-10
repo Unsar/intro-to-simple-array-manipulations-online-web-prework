98e6039056952133767e4c616317cc61eb97be95
Unsar Malik <unsar.malik24@gmail.com>
2019-04-10 02:37:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,14 +23,13 @@ def using_shift(my_favorite_cities)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
     my_favorite_cities.shift
   end
-end
     
-  def shift_with_args(ice_cream_brands)
+def shift_with_args(ice_cream_brands)
     ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
     ice_cream_brands.shift(2)
   end
     
-  def using_concat(my_favorite_things, more_favs)
+def using_concat(my_favorite_things, more_favs)
     my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
     more_favs = ["mario kart", "flatiron school"]
     my_favorite_things.concat(more_favs)
